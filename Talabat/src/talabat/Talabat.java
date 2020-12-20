6ccea3be4d319b96ed62bf10b62304d6489e281c
minab <minab@Mina-PC>
2020-12-20 17:29:47 +0200

    edited the main class

--- Talabat/src/talabat/Talabat.java
+++ Talabat/src/talabat/Talabat.java
@@ -1,23 +1,9 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package talabat;
 
-/**
- *
- * 
- */
 public class Talabat {
 
-    /**
-     * @param args the command line arguments
-     */
     public static void main(String[] args) {
-        // TODO code application logic here
-        //fdasfasd sad
-        ///commit2
+        
     }
     
 }
