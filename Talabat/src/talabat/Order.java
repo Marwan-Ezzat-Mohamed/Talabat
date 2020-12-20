07be78449fe9f46106e2d2cdfbad4262401d7b0a
nouran <nouran@192.168.1.15>
2020-12-20 21:26:50 +0200

    hehe

---
+++ Talabat/src/talabat/Order.java
@@ -0,0 +1,18 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package talabat;
+
+/**
+ *
+ * @author nouran
+ */
+public class Order {
+    int ordererdMeal;
+    int quantity;
+    String notes;
+    String Date;
+ 
+}
